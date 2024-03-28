f7ca7fae1210f67f7f6d5583d55adc086eab5f94
Humoyin <baxranoofxumoyun@gmail.com>
2024-03-28 14:14:42 +0500

    yananimadir

--- app.js
+++ app.js
@@ -6,11 +6,20 @@ console.log(age)
 console.log(yosh)
 var yosh=23
 //funksiyalar
+//function decloration
 function seyHello(){
     console.log("salom humoyun")
 }
+//odiy funksiyaqada buni farqi yuq
 seyHello()
+//function expression
 const seyByebye=function (){
     console.log("hayr osiyo")
 }
+// constanta orqali birilgan funksiyalar hardoim tagida chaqirilishi kirak
 seyByebye()
+//arraw funktion
+const nice=()=>{
+    console.log("this is arraw function")
+}
+nice()
