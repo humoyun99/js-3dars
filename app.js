144ff0964a91c12166a3b193cc1435aafe2b87aa
Humoyin <baxranoofxumoyun@gmail.com>
2024-03-28 14:07:27 +0500

    uzgarish

--- app.js
+++ app.js
@@ -5,3 +5,12 @@ console.log(age)
 //bu hato narsa 
 console.log(yosh)
 var yosh=23
+//funksiyalar
+function seyHello(){
+    console.log("salom humoyun")
+}
+seyHello()
+const seyByebye=function (){
+    console.log("hayr osiyo")
+}
+seyByebye()
